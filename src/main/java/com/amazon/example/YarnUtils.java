f0eee5a89e9fb53643b5354de59e300b618b344a
root <root@ip-172-31-73-196.ec2.internal>
2023-12-22 03:41:40 +0000

    fixed yarnUtils

--- src/main/java/com/amazon/example/YarnUtils.java
+++ src/main/java/com/amazon/example/YarnUtils.java
@@ -3,8 +3,12 @@ package com.amazon.example;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.List;
+import java.util.HashMap;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
@@ -21,7 +25,12 @@ import org.apache.hc.core5.http.io.HttpClientResponseHandler;
 import org.apache.http.protocol.HTTP;
 import org.xml.sax.SAXException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class YarnUtils {
+	private Logger logger = LoggerFactory.getLogger(YarnUtils.class);
 
 	class myHandlerClass<T> implements HttpClientResponseHandler<Document> {
 		@Override
@@ -64,6 +73,7 @@ public class YarnUtils {
 
 	private List<String> rmPrivateNames = null;
 	private EmrUtils emr = new EmrUtils();
+	private static HashMap<String,String> yarnConfigMap = null;
 
 	public YarnUtils() {
 		this.rmPrivateNames = emr.getMastersPrivateNames();
@@ -71,23 +81,45 @@ public class YarnUtils {
 	}
 	
 	protected String getRMProxy() {
-		Document yarnConfig = this.getRMconfig();
-		yarnConfig.getDocumentElement().normalize();
-
-        return "Root element: " + yarnConfig.getDocumentElement().getNodeName();
+		HashMap<String,String> yarnConfig = this.getRMconfig();
+        	return yarnConfig.get("yarn.web-proxy.address");
 	}
 
-	protected Document getRMconfig() {
-		Document result = null;
-		try {
-			result = Request.get("http://" + rmPrivateNames.get(0) + ":8088/conf").execute()
-					.handleResponse(new myHandlerClass<Document>());
+	protected HashMap<String,String> getRMconfig() {
+		if (YarnUtils.yarnConfigMap == null){
+			Document result = null;
+				try {
+					result = Request.get("http://" + rmPrivateNames.get(0) + ":8088/conf").execute()
+						.handleResponse(new myHandlerClass<Document>());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			result.getDocumentElement().normalize();
+        	        NodeList nList = result.getElementsByTagName("property");
+
+	                for (int i = 0; i < nList.getLength(); i++) {
+                	        Node nNode = nList.item(i);
+                        	if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                                	Element elem = (Element) nNode;
+                                	Node node1 = elem.getElementsByTagName("source").item(0);
+                                	String src = node1.getTextContent();
+
+                                	if (src.equals("yarn-site.xml") || src.equals("yarn-default.xml")){
+                                        	Node node2 = elem.getElementsByTagName("name").item(0);
+                                        	String name = node2.getTextContent();
+                                        	Node node3 = elem.getElementsByTagName("value").item(0);
+                                        	String value = node3.getTextContent();
+						if (YarnUtils.yarnConfigMap == null) {
+							YarnUtils.yarnConfigMap = new HashMap<String,String>();
+						}
+						YarnUtils.yarnConfigMap.put(name,value);
+                                        	logger.info(src + " : " + name + " : " + value );
+                                	}
+                        	}
+                	}
 		}
-		return result;
-
+		return YarnUtils.yarnConfigMap;
 	}
 }
